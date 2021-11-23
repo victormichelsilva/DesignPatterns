6c4181718c66437ee3294914e4736d72045525f7
victormichelsilva <victormichelsilva@gmail.com>
2021-11-23 10:05:50 -0300

    Adding files

---
+++ Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Cliente 
+    {
+        // campos
+        public string nome;
+
+        // métodos
+        public Cliente(string nome, int idade) 
+        {
+            this.nome = nome;
+        }
+
+        public void Saudacao() 
+        {
+            Console.WriteLine("- Olá, meu nome é: " + nome);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente("Victor", 33);
+            //cliente.nome = "Victor";
+            cliente.Saudacao();
+
+            //Console.WriteLine(cliente.nome);
+        }
+    }
+}
